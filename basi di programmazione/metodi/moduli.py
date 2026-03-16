c1b7dd424e58ff6bae3776e3ab368a609e62d811
riccardogouthier <riccardogouthier@gmail.com>
2026-03-16 15:53:41 +0100

    inshallah

--- basi di programmazione/metodi/moduli.py
+++ basi di programmazione/metodi/moduli.py
@@ -1,27 +1,22 @@
 import requests
 
 # url = 'https://pokeapi.co/api/v2/pokemon/'
-
 url = 'https://api.tvmaze.com/singlesearch/shows?q='
 
+
+
 cerca = input("Scrivi il titolo di tv-show: ")
-
-
-
 risposta = requests.get(url + cerca)
 
 if risposta.status_code == 200:
     show = risposta.json()
-    #  print(type(show))
+
     titolo = show.get('name')
     rating = show.get('rating')
     img = show.get('image')
-
-
+    # print(type(show))
     print(titolo, rating['average'], img['medium'])
     
-
-
 # if risposta.status_code == 200:
 #     pokemon = risposta.json()
 #     print(type(pokemon))
