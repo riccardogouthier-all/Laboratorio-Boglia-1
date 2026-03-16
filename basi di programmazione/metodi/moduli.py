73528c6d4a13032fd76cd2e938169874a27581ce
riccardogouthier <riccardogouthier@gmail.com>
2026-03-16 15:36:09 +0100

    inshallah

--- basi di programmazione/metodi/moduli.py
+++ basi di programmazione/metodi/moduli.py
@@ -3,6 +3,15 @@ import requests
 url = 'https://pokeapi.co/api/v2/pokemon/'
 
 
-risposta = requests.get(url + 'pikachu')
+risposta = requests.get(url + 'pichu')
 
-print(risposta.status_code)
+
+if risposta.status_code == 200:
+    pokemon = risposta.json()
+    print(type(pokemon))
+
+
+    for chiave, valore in pokemon.items():
+        print(f"{chiave}, {valore}")
+        print("----------------------------------------------------")
+
