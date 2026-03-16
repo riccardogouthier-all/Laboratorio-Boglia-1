4be702f51ca6e41948cbbdc2e29aadd5adad3a83
riccardogouthier <riccardogouthier@gmail.com>
2026-03-16 15:51:32 +0100

    inshallah

--- basi di programmazione/metodi/moduli.py
+++ basi di programmazione/metodi/moduli.py
@@ -1,17 +1,33 @@
 import requests
 
-url = 'https://pokeapi.co/api/v2/pokemon/'
+# url = 'https://pokeapi.co/api/v2/pokemon/'
+
+url = 'https://api.tvmaze.com/singlesearch/shows?q='
+
+cerca = input("Scrivi il titolo di tv-show: ")
 
 
-risposta = requests.get(url + 'pichu')
 
+risposta = requests.get(url + cerca)
 
 if risposta.status_code == 200:
-    pokemon = risposta.json()
-    print(type(pokemon))
+    show = risposta.json()
+    #  print(type(show))
+    titolo = show.get('name')
+    rating = show.get('rating')
+    img = show.get('image')
 
 
-    for chiave, valore in pokemon.items():
-        print(f"{chiave}, {valore}")
-        print("----------------------------------------------------")
+    print(titolo, rating['average'], img['medium'])
+    
+
+
+# if risposta.status_code == 200:
+#     pokemon = risposta.json()
+#     print(type(pokemon))
+
+
+#     for chiave, valore in pokemon.items():
+#         print(f"{chiave}, {valore}")
+#         print("----------------------------------------------------")
 
