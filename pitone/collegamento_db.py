6df85dcdfa1e8d4fb9aa3e0e061016b7ee7f7260
riccardogouthier <riccardogouthier@gmail.com>
2026-03-30 12:48:54 +0200

    commit

--- pitone/collegamento_db.py
+++ pitone/collegamento_db.py
@@ -1,4 +1,11 @@
 import mysql.connector
+from dataclasses import dataclass
+
+@dataclass
+class Prodotto:
+    nome : str
+    prezzo : float
+    giacenza : int
 
 magazzino = mysql.connector.connect(
     host = 'localhost',
@@ -9,11 +16,26 @@ magazzino = mysql.connector.connect(
 
 cursore = magazzino.cursor()
 
-query = "select * from prodotti"
+query = "select nome, prezzo_unitario, quantita_stock from prodotti"
 
 cursore.execute(query)
 
-prodotti = cursore.fetchall()
+prodotti = cursore.fetchall()           #preleva gli elementi letti
 
-print(prodotti)
+# tendina = []
 
+tendina = [Prodotto(n,p,q) for n, p, q in prodotti]
+
+# for p in prodotti:
+#     tendina.append(p[0])
+
+with open("Esercitazione_magazzino.txt", "w") as f:
+    f.write(query)
+    
+    print(query)
+    for p in tendina:
+        # f.write(f"{row.nome:30}{row.prezzo:10}{row.giacenza:10} '\n'") # per elementi in una lista
+        f.write(f"{p.nome:30} {p.prezzo:10} {p.giacenza:10} '\n'") # per elementi in una lista
+
+
+ 
