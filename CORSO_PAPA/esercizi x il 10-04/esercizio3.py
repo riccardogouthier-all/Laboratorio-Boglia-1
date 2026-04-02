9e2b4816c07164be5585ef944d49e2ecf48f68f7
riccardogouthier <riccardogouthier@gmail.com>
2026-04-02 11:32:12 +0200

    commit

--- CORSO_PAPA/esercizi x il 10-04/esercizio3.py
+++ CORSO_PAPA/esercizi x il 10-04/esercizio3.py
@@ -7,16 +7,25 @@ temperatura espressa in gradi Fahrenheit in una temperatura espressa in gradi Ce
 Usare la seguente formula: C = (F   - 32) * 5 / 9 
 Creare un programma principale che richiami la funzione e ne stampi il risultato visualizzando solo 3 cifre decimali. 
 '''
+def fahrenheit():
+    """ ciclo per controllare se il numero inserito per le ore non è negativo o un valore alfabetico """
+    a = input("Inserisci qui i gradi Fahrenheit: ")
+    while not a.lstrip('-').isdigit():
+        a = input("Inserisci qui il NUMERO di Fahrenheit CORRETTO (FORMATO NUMERICO): ")
+        ore = int(a)
+    return ore
 
+def conversione(F):
+    C = (F   - 32) * 5 / 9
+    return C
 
 def main():
-    pass
+    fahr = fahrenheit()
+    celsius = conversione(fahr)
+    print(fahr)
 
 if __name__ == "__main__":
     main()
-
-
-
 
 # metodo per gestire gli input
 
@@ -27,9 +36,3 @@ if __name__ == "__main__":
 # Assegnazione valori dopo il calcolo dei secondi
 
 # Ciclo per confrontare i valori + selezione output
-
-
-
-
-
-
