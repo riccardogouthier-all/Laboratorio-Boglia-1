3a90de0f0fdf9cf6ec0fa7aa251583b9eee31811
riccardogouthier <riccardogouthier@gmail.com>
2026-04-03 11:44:34 +0200

    commit

--- CORSO_PAPA/esercizi x il 10-04/esercizio4.py
+++ CORSO_PAPA/esercizi x il 10-04/esercizio4.py
@@ -4,7 +4,7 @@ Data: 26/03/2026
 Titolo: 
 La seguente è la formula per valutare numericamente il numero di Nepero e:
         e:= sum n=0 to ∞ di { 1/(n!) = 1 0! + 1 1! +...+ 1 n! +... }
-        
+
 4.a Scrivere una funzione che restituisca un valore approssimato di e all'ennesimo termine, dove N è inserito come parametro formale alla funzione.  
     Esempio: calcola_e(3) restituirà il valore di e calcolato con tre termini della (1), cioè:
             e= 1/0! + 1/1! + 1/2! = 1 + 1 + 0.5 = 2.5 
@@ -16,19 +16,16 @@ La seguente è la formula per valutare numericamente il numero di Nepero e:
     Suggerimento: la funzione calcola_e(N) dovrà restituire due valori,  2.718281828459045 potrebbe essere memorizzato in una variabile globale. 
     esempio: valuta_e(3) restituisce il valore calcolato nel punto 4.a 2.5 e 0,218281828459045 che rappresenta la differenza tra 2.718281828459045 e 2.5 
 '''
- 
-# metodo per gestire gli input
 
-# metodo per calcolare il numero di secondi dagli input
+import math
 
-# Inizializzazioni variabili
+# Metodo 1: Costante predefinita
+e_val = math.e
+print(f"Valore di e (math.e): {e_val}")
 
-# Assegnazione valori dopo il calcolo dei secondi
-
-# Ciclo per confrontare i valori + selezione output
-
-
-
-
+# Metodo 2: Serie di Taylor (approssimazione)
+n = 20 # Numero di termini
+e_approx = sum(1/math.factorial(i) for i in range(n))
+print(f"Valore di e (serie Taylor): {e_approx}")
 
 
