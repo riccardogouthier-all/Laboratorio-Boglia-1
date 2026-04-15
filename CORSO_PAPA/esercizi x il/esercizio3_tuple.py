b673bc24cc072fa2e715404a2a53a6f3096e68ce
Riccardo Gouthier <riccardogouthier@gmail.com>
2026-04-15 22:14:21 +0200

    commit

--- CORSO_PAPA/esercizi x il/esercizio3_tuple.py
+++ CORSO_PAPA/esercizi x il/esercizio3_tuple.py
@@ -9,10 +9,20 @@ TuplaOUT: ([10, 20, 100], [40, 50, 100], [70, 80, 100])
 
 
 def controllo_liste_tupla(tupla):
+    """
+    Verifica che tutti gli elementi della tupla siano liste.
+    In ingresso => tupla: Tupla da verificare
+    Ritorna => bool: True se tutti gli elementi sono liste, False altrimenti
+    """
     permesso = all(isinstance(elemento, list) for elemento in tupla)
     return permesso
 
 def sostituisci_ultimo_valore(tupla_liste, valore):
+    """
+    Sostituisce l'ultimo elemento di ogni lista nella tupla con nuovo_valore.
+    In ingresso => tupla_liste: Tupla contenente SOLO liste E nuovo_valore: Valore da inserire come ultimo elemento
+    Ritorna => Tupla con le liste modificate o None se la tupla non contiene solo liste
+    """
 
     nuovi_elemnti = []
     for lista in tupla_liste:
@@ -29,5 +39,5 @@ if not controllo_liste_tupla(TuplaIN):
 else:
     if any(len(lista) == 0 for lista in TuplaIN):
         print("La tupla contiene almeno una lista vuota")
-    if controllo_liste_tupla(TuplaIN) and not any(len(lista) == 0 for lista in TuplaIN):
+    else:
         print(sostituisci_ultimo_valore(TuplaIN, valore))
