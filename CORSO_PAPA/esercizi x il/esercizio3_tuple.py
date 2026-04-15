bf7951cd95fe358e5dfb605c1bbc480b47ddcfe7
Riccardo Gouthier <riccardogouthier@gmail.com>
2026-04-15 21:42:10 +0200

    commit

--- CORSO_PAPA/esercizi x il/esercizio3_tuple.py
+++ CORSO_PAPA/esercizi x il/esercizio3_tuple.py
@@ -9,20 +9,16 @@ TuplaOUT: ([10, 20, 100], [40, 50, 100], [70, 80, 100])
 
 
 
+def sostituisci_ultimo_valore(tupla_liste, valore):
 
+    nuovi_elemnti = []
+    for lista in tupla_liste:
+        nuovo_elemnto = lista[:-1] + [valore]
+        nuovi_elemnti.append(nuovo_elemnto)
+    return nuovi_elemnti
 
+valore = 100 
+TuplaIN = ([10, 20, 40], [40, 50, 60], [70, 80, 90])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(TuplaIN)
+print(sostituisci_ultimo_valore(TuplaIN, valore))
