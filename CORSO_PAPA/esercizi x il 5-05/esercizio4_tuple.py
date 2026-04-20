6a13dfa6c51bcedf085b4a5e7d659924d366c6bd
riccardogouthier <riccardogouthier@gmail.com>
2026-04-20 17:26:19 +0200

    commit

--- CORSO_PAPA/esercizi x il 5-05/esercizio4_tuple.py
+++ CORSO_PAPA/esercizi x il 5-05/esercizio4_tuple.py
@@ -1,7 +1,7 @@
 '''
 Quarto Esercizio 
 Scrivere un programma per contare gli elementi in una lista finché non si incontra un elemento di tipo tupla. 
-[Suggerimento: si usi la funzione isinstance( ) ] 
+[Suggerimento: si usi la funzione isinstance()] 
 '''
 
     
