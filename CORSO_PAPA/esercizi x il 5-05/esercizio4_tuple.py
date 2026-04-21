54282a2b3dd3f9ce45e8a1cbe83020d6cd809f30
riccardogouthier <riccardogouthier@gmail.com>
2026-04-21 09:57:43 +0200

    COMMT

--- CORSO_PAPA/esercizi x il 5-05/esercizio4_tuple.py
+++ CORSO_PAPA/esercizi x il 5-05/esercizio4_tuple.py
@@ -4,21 +4,21 @@ Scrivere un programma per contare gli elementi in una lista finché non si incon
 [Suggerimento: si usi la funzione isinstance()] 
 '''
 
-    
 
 
+lista = [ "a" , 1 , ["a","b","c"] , "a"  , ["a","b","c"] , 1 , "a" , ["a","b","c"] , 1 , "a" , ["a","b","c"] , 1 , ["a","b","c"] , "a" , 1 , "a" , 1 , ("LA TUPLA SONO IO",4) ]
+print(lista)
 
+contatore = 0
 
-
-
-
-
-
-
-
-
-
-
+for elemento in lista:
+    if isinstance(elemento, tuple):
+        break
+    contatore += 1
+if contatore == len(lista):
+    print(f"Nella lista non sono presenti elementi di tipo tupla e contiene {len(lista)} elementi.")
+else:       
+    print(f"Nella lista ci sono {contatore} elementi prima di incontrare una tupla")
 
 
 
