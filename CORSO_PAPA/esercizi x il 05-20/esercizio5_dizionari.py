4b3317963ac2f3a04a0b899be94f99464e86f742
Riccardo Gouthier <riccardogouthier@gmail.com>
2026-05-17 18:08:01 +0200

    commit

--- CORSO_PAPA/esercizi x il 05-20/esercizio5_dizionari.py
+++ CORSO_PAPA/esercizi x il 05-20/esercizio5_dizionari.py
@@ -3,10 +3,10 @@ Quinto esercizio
 Scrivete un programma Python per creare un dizionario da una stringa. Le lettere della stringa rappresentano le chiavi, i valori rappresentano le occorrenze della chiave nella stringa 
 '''
 
-def crea_dizionario_da_stringa(inp):
+def crea_dizionario_da_stringa(string):
     """Crea un dizionario contando le occorrenze di ogni lettera nella stringa"""
     dizionario = {}
-    stringa = "".join(sorted(inp.replace(" ","").replace(",","").replace("'","").replace("!","").replace("?","").replace(".","").lower()))
+    stringa = "".join(sorted(string.replace(" ","").replace(",","").replace("'","").replace("!","").replace("?","").replace(".","").lower()))
     for lettera in stringa:
         if lettera in dizionario:
             dizionario[lettera] += 1  # incrementa il conteggio
