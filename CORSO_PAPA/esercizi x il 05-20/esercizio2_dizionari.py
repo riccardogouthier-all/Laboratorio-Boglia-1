5018bf94bcc8db26c59fdb9673ddf45e125f4c42
Riccardo Gouthier <riccardogouthier@gmail.com>
2026-05-17 16:34:58 +0200

    commit

--- CORSO_PAPA/esercizi x il 05-20/esercizio2_dizionari.py
+++ CORSO_PAPA/esercizi x il 05-20/esercizio2_dizionari.py
@@ -3,10 +3,10 @@ Secondo esercizio
 Scrivete uno script Python per generare e stampare un dizionario che contenga un numero (compreso tra 1 e n) nella forma (x, x*x). 
 '''
 
-n = -5
+n = 5
 
-if n <= 0:
-    print("n <= 0, cambiare il valore in input")
+if n <= 0 or not isinstance(n, int):
+    print("Cambiare il valore in input in un numero intero e maggiore di 0")
 else:
     dizionario = {}
     for x in range(1, n + 1):
