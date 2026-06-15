458eec668763fbd8e283dc2e7b9211a9e479bcdd
riccardogouthier-all <riccardogouthier@gmail.com>
2026-06-15 09:37:20 +0200

    commit

--- CORSO_PAPA/esercizi x il 06-17/esercizio2_moduloOS.py
+++ CORSO_PAPA/esercizi x il 06-17/esercizio2_moduloOS.py
@@ -2,3 +2,6 @@
 Scrivere un programma che esamini tutte le directories sotto un dato percorso e conti tutti i files con una determinata estensione data in input.
 [In prima battuta fermatevi al primo livello di profondità delle directories]
 '''
+
+
+
